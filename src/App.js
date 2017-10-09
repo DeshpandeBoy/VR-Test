af94dfbb31b8477a4374b9f929581049ba91f01c
DeshpandeBoy <samiradeshpande10@gmail.com>
2017-10-09 15:38:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,72 +1,94 @@
 import React, { Component } from "react";
-import "aframe";
 
-class Home extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
       data: [],
-      gallery: true,
-      currentImage: null
+      value: "",
+      filteredList: []
     };
   }
 
   componentDidMount() {
-    fetch("https://demo0813639.mockable.io/getPanos")
+    fetch(
+      "https://app.fyle.in/api/bank_branches?city=BANGALORE&offset=0&limit=50"
+    )
       .then(result => result.json())
       .then(data => this.setState({ data }));
   }
 
-  handleClick(pano) {
+  _handleChange(event) {
     this.setState({
-      gallery: false,
-      currentImage: pano
+      value: event.target.value
     });
-    console.log(this.state.currentImage);
+    this._filter(event.target.value.toUpperCase(), this.state.data);
+  }
+
+  _filter(input, data) {
+    let filteredList = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].bank_name.includes(input)) {
+        filteredList.push(data[i]);
+        this.setState({ filteredList });
+      }
+    }
   }
 
   render() {
-    const vr = this.state.gallery;
-
     return (
-      <div>
-        {vr ? (
-          <div className="container">
-            <h1>Click any of the pic to experience vr</h1>
-            <div className="images">
-              <ul id="images">
-                {this.state.data.map((image, i) => (
-                  <img
-                    key={i}
-                    className="col-3"
-                    src={image.pano}
-                    onClick={() => this.handleClick(image.pano)}
-                  />
-                ))}
-              </ul>
-              <ul id="text">
-                {this.state.data.map((name, i) => (
-                  <h1 key={i} className="col-3">
-                    {" "}
-                    {name.name}{" "}
-                  </h1>
-                ))}
-              </ul>
-            </div>
+      <div className="container">
+        <div id="dropdown">
+          <select>
+            {this.state.data.map((bank, i) => (
+              <option key={i}>{bank.city}</option>
+            ))}
+          </select>
+          <div id="search">
+            <input onChange={e => this._handleChange(e)} />
           </div>
-        ) : (
-          <div>
-            <button onClick={() => this.setState({ gallery: true })}>
-              Go back
-            </button>
-            <a-scene>
-              <a-sky src={this.state.currentImage}> </a-sky>)
-            </a-scene>
+          <div id="table">
+            <table className="table-striped table-hover">
+              <thead className="thead-inverse">
+                <th>IFSC</th>
+                <th>Bank Name</th>
+                <th>Branch</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>District</th>
+                <th>State</th>
+              </thead>
+              <tbody>
+                {this.state.value.length !== 0
+                  ? this.state.filteredList.map((bank, i) => (
+                      <tr key={i}>
+                        <td>{bank.ifsc}</td>
+                        <td>{bank.bank_name}</td>
+                        <td>{bank.branch}</td>
+                        <td>{bank.address}</td>
+                        <td>{bank.city}</td>
+                        <td>{bank.district}</td>
+                        <td>{bank.state}</td>
+                      </tr>
+                    ))
+                  : this.state.data.map((bank, i) => (
+                      <tr key={i}>
+                        <td>{bank.ifsc}</td>
+                        <td>{bank.bank_name}</td>
+                        <td>{bank.branch}</td>
+                        <td>{bank.address}</td>
+                        <td>{bank.city}</td>
+                        <td>{bank.district}</td>
+                        <td>{bank.state}</td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
           </div>
-        )}
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+export default App;
